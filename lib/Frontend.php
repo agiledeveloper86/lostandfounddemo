52d850884b0b0472b4a4cba1ab0fe8d16f3d9f3f
agiledeveloper86 <friendz4ever86@gmail.com>
2012-12-12 14:55:10 +0530

    upload for lostandfound-1n2

--- lib/Frontend.php
+++ lib/Frontend.php
@@ -2,26 +2,77 @@
 
 //creating Frontend child from ApiFrontend parent
 class Frontend extends ApiFrontend {
-    function init(){
+    function init() {
         parent::init();
+        
 
-//making connection with db        
+//connecting with db
         $this->dbConnect();
 
-//to include agiletoolkit
+//not so mucch important ---->for adding atk new version
         $this->requires('atk','4.2.0');
 
-//adding jQuery features
+
+//add jquery features
         $this->add('jUI');
 		
-		
-//adding menu menu		
-		$this->add('Menu',null,'Menu')
 
-//adding meu items
-            ->addMenuItem('index','Welcome')
-            ->addMenuItem('index', 'logout')
-             ;
+//adding authentication feature
+	   $this->add('Auth')->setModel('User');
+
+//witelistening from authentication
+       $this->auth->allowPage(array('Register', 'index'));
+
+
+       
+    	
+//menu for logged in user	   
+		if($this->auth->isLoggedIn()){ 
+
+
+        //initial menu for logged in user
+        $menu = $this->add('Menu',null,'Menu')
+            ->addMenuItem('index', 'Welcome')
+            ->addMenuItem('items/lost', 'Report a Lost Item')
+            ;
+            
+
+        //exta menu for admin user not showing to the other logged in user    
+            $is_admin = $this->api->auth->model['is_admin'];
+            if($is_admin){
+                $menu->addMenuItem('Admin');
+            }
+            
+        //appending other extra menu for logged in user    
+            $menu->addMenuItem('logout');
+        
+
+        //menu for general user
+        } else {
+
+
+            $menu = $this->add('Menu',null,'Menu')
+            ->addMenuItem('index', 'Welcome')
+            ->addMenuItem('Login')
+            ->addMenuItem('Register')
+            ;
+
+        }
+    
+
+
+   //checking for authentication
+   $this->auth->check();
+
+
+    
+
+
+
+             
+
+
+        
 
     }
 }
--- page/index.php
+++ page/index.php
@@ -1,9 +1,21 @@
 <?php
 
-//creating page_index child from Page parent
+//creating page_index child from page parent
 class page_index extends Page {
 	function init(){
 		parent::init();
+		$page = $this;
+
+		
+
+
+//creating welcome screen for logged in user
+		if($this->api->auth->isLoggedIn()){
+
+            $welcome = $this->add('View_Info')->set('Hello, ' .$this->api->auth->model['first_name']);
+            $welcome = $welcome->add('Text')->set('.....');
+
+		}
 
 
 		
